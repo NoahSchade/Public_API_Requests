b6ad8403399c0021249bdbc04c359ceeab2332cc
Noah Schade <noah9@mail.com>
2019-04-12 11:23:13 -0400

    Show modal when card is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,13 @@ $('.search-container').html(`
 
 fetch('https://randomuser.me/api/?results=12')
     .then(response => response.json())
-    .then(data => generateImage(data))
+    .then(data => generateData(data))
+    .then(() => addModal())
 
 let html = '';
 let htmlHolder = '';
 
-function generateImage(data) {
+function generateData(data) {
     
 console.log(data.results[0].picture.large);
 
@@ -34,56 +35,59 @@ console.log(data.results[0].picture.large);
     $('#gallery').html(html);
 }
 
-$("body").append("<div class='modal-large-container'>Appended text</div>");
-// $(".modal-large-container").hide();
+function addModal() {
+    $("body").append("<div class='modal-large-container'>Appended text</div>");
+    $(".modal-large-container").hide();
 
-$('.modal-large-container').html(`
-    <div class="modal-container">
-        <div class="modal">
-            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-            <div class="modal-info-container">
-                <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-                <h3 id="name" class="modal-name cap">name</h3>
-                <p class="modal-text">email</p>
-                <p class="modal-text cap">city</p>
-                <hr>
-                <p class="modal-text">(555) 555-5555</p>
-                <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-                <p class="modal-text">Birthday: 10/21/2015</p>
+    $('.modal-large-container').html(`
+        <div class="modal-container">
+            <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                    <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+                    <h3 id="name" class="modal-name cap">name</h3>
+                    <p class="modal-text">email</p>
+                    <p class="modal-text cap">city</p>
+                    <hr>
+                    <p class="modal-text">(555) 555-5555</p>
+                    <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+                    <p class="modal-text">Birthday: 10/21/2015</p>
+                </div>
+            </div>
+
+            <div class="modal-btn-container">
+                <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                <button type="button" id="modal-next" class="modal-next btn">Next</button>
             </div>
         </div>
+    `);
 
-        <div class="modal-btn-container">
-            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-            <button type="button" id="modal-next" class="modal-next btn">Next</button>
-        </div>
-    </div>
-`);
-
-$("#modal-close-btn").click(function(){
-    $(".modal-large-container").hide();
-});
-
-$(".card").click(function(){
-    $(".modal-large-container").show();
-});
-
-hiddenElements = $(':hidden');
-visibleElements = $(':visible');
-
-let i = false
-
-$(".modal").click(function(){
-    i = true;
-});
-
-$(".modal-btn-container").click(function(){
-    i = true;
-});
-
-$(".modal-large-container").click(function(){
-    if(i === false) {
+    $("#modal-close-btn").click(function(){
         $(".modal-large-container").hide();
-    }
-    i = false;
-});
+    });
+
+    $(".card").click(function(){
+        $(".modal-large-container").show();
+    });
+
+    hiddenElements = $(':hidden');
+    visibleElements = $(':visible');
+
+    let i = false
+
+    $(".modal").click(function(){
+        i = true;
+    });
+
+    $(".modal-btn-container").click(function(){
+        i = true;
+    });
+
+    $(".modal-large-container").click(function(){
+        if(i === false) {
+            $(".modal-large-container").hide();
+        }
+        i = false;
+    });
+
+}
