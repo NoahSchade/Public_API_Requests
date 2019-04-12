bedf4121a2c63b9b7242bc6b5bc64bebebeae31d
Noah Schade <noah9@mail.com>
2019-04-12 18:41:22 -0400

    Remove state from directory

--- js/scripts.js
+++ js/scripts.js
@@ -23,7 +23,7 @@ function generateData(data) {
                         <div class="card-info-container">
                             <h3 id="name" class="card-name cap">${data.results[i].name.first} ${data.results[i].name.last}</h3>
                             <p class="card-text">${data.results[i].email}</p>
-                            <p class="card-text cap">${data.results[i].location.city}, ${data.results[i].location.state}</p>
+                            <p class="card-text cap">${data.results[i].location.city}</p>
                         </div>
                     </div>
                 `;
